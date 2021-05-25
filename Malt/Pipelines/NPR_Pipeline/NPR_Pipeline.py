b6bdaa45fc2cb0803b09ddb3c2099f69d79fb1bd
pragma37 <pragma37@gmail.com>
2021-05-26 00:20:36 +0200

    light and screen shaders

--- Malt/Pipelines/NPR_Pipeline/NPR_Pipeline.py
+++ Malt/Pipelines/NPR_Pipeline/NPR_Pipeline.py
@@ -31,6 +31,51 @@ void COMMON_PIXEL_SHADER(Surface S, inout PixelOutput PO)
 }
 '''
 
+_DEFAULT_LIGHT_SHADER_SRC='''
+#include "Pipelines/NPR_Pipeline.glsl"
+
+void LIGHT_SHADER(LightShaderInput I, inout LightShaderOutput O) { }
+'''
+
+_SCREEN_SHADER_SRC='''
+#include "Pipelines/NPR_Pipeline.glsl"
+
+#ifdef PIXEL_SHADER
+
+uniform sampler2D INPUT_0; uniform sampler2D INPUT_1; uniform sampler2D INPUT_2; uniform sampler2D INPUT_3; 
+uniform sampler2D INPUT_4; uniform sampler2D INPUT_5; uniform sampler2D INPUT_6; uniform sampler2D INPUT_7;
+
+layout (location = 0) out vec4 OUTPUT_0; layout (location = 1) out vec4 OUTPUT_1;
+layout (location = 2) out vec4 OUTPUT_2; layout (location = 3) out vec4 OUTPUT_3;
+layout (location = 4) out vec4 OUTPUT_4; layout (location = 5) out vec4 OUTPUT_5;
+layout (location = 6) out vec4 OUTPUT_6; layout (location = 7) out vec4 OUTPUT_7;
+
+void SCREEN_SHADER(vec2 uv, 
+    sampler2D Input_0, sampler2D Input_1, sampler2D Input_2, sampler2D Input_3,
+    sampler2D Input_4, sampler2D Input_5, sampler2D Input_6, sampler2D Input_7,
+    out vec4 Output_0, out vec4 Output_1, out vec4 Output_2, out vec4 Output_3,
+    out vec4 Output_4, out vec4 Output_5, out vec4 Output_6, out vec4 Output_7);
+
+void main()
+{
+    SCREEN_SHADER(UV[0], 
+        INPUT_0, INPUT_1, INPUT_2, INPUT_3, INPUT_4, INPUT_5, INPUT_6, INPUT_7, 
+        OUTPUT_0, OUTPUT_1, OUTPUT_2, OUTPUT_3, OUTPUT_4, OUTPUT_5, OUTPUT_6, OUTPUT_7
+    );
+}
+
+#endif //PIXEL_SHADER
+'''
+
+_DEFAULT_SCREEN_SHADER_SRC= _SCREEN_SHADER_SRC + '''
+void SCREEN_SHADER(vec2 uv, 
+    sampler2D Input_0, sampler2D Input_1, sampler2D Input_2, sampler2D Input_3,
+    sampler2D Input_4, sampler2D Input_5, sampler2D Input_6, sampler2D Input_7,
+    out vec4 Output_0, out vec4 Output_1, out vec4 Output_2, out vec4 Output_3,
+    out vec4 Output_4, out vec4 Output_5, out vec4 Output_6, out vec4 Output_7)
+{ }
+'''
+
 _BLEND_TRANSPARENCY_SHADER = None
 
 _BLEND_TRANSPARENCY_SHADER_SRC='''
@@ -100,6 +145,23 @@ class NPR_Pipeline(Pipeline):
             'COMMON_VERTEX_SHADER': ('CUSTOM_VERTEX_SHADER', 'void COMMON_VERTEX_SHADER(inout Surface S)'),
         }, 
         '#include "Pipelines/NPR_Pipeline.glsl"')
+
+        source = self.preprocess_shader_from_source(_DEFAULT_LIGHT_SHADER_SRC, [], ['IS_LIGHT_SHADER','PIXEL_SHADER','NODES'])
+        self.graphs['Light Shader'] = GLSLPipelineGraph('.light.glsl', source, SHADER_DIR, {
+            'LIGHT_SHADER': (None, 'void LIGHT_SHADER(LightShaderInput I, inout LightShaderOutput O)')
+        },
+        '#include "Pipelines/NPR_Pipeline.glsl"')
+
+        source = self.preprocess_shader_from_source(_DEFAULT_SCREEN_SHADER_SRC, [], ['IS_SCREEN_SHADER','PIXEL_SHADER','NODES'])
+        self.graphs['Screen Shader'] = GLSLPipelineGraph('.screen.glsl', source, SHADER_DIR, {
+            'SCREEN_SHADER': (None, '''void SCREEN_SHADER(vec2 uv, 
+            sampler2D Input_0, sampler2D Input_1, sampler2D Input_2, sampler2D Input_3,
+            sampler2D Input_4, sampler2D Input_5, sampler2D Input_6, sampler2D Input_7,
+            out vec4 Output_0, out vec4 Output_1, out vec4 Output_2, out vec4 Output_3,
+            out vec4 Output_4, out vec4 Output_5, out vec4 Output_6, out vec4 Output_7);''')
+        },
+        _SCREEN_SHADER_SRC)
+        
 
     def compile_material_from_source(self, material_type, source, include_paths=[]):
         if material_type == 'mesh':
