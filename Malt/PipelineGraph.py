3562e37c85cc409cc10012b7393237a46c31d2d0
pragma37 <pragma37@gmail.com>
2021-06-17 17:21:45 +0200

    Delete PipelineGraph.py

--- Malt/PipelineGraph.py
+++
@@ -1,7 +0,0 @@
-'''
-Transpiler -> Higher Level -> function(name, type, parameters)
-
-Reflection -> Common type (no dict) -> parameter(name, malt_type, source_type, source_path, default_value)
-
-New Parameter Type -> Callback || Graph
-'''
