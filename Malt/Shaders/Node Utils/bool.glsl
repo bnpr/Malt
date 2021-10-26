da4e1ede9a8ec0c5a79bce79fccbc03ef29ae578
pragma37 <pragma37@gmail.com>
2021-10-26 22:59:15 +0200

    basic bvec utils

--- Malt/Shaders/Node Utils/bool.glsl
+++ Malt/Shaders/Node Utils/bool.glsl
@@ -14,4 +14,13 @@ bool bool_not_equal(bool a, bool b){ return a != b; }
 
 bool if_else(bool condition, bool a, bool b){ return condition ? a : b; }
 
+void bvec2_split(bvec2 v, out bool x, out bool y){ x = v.x; y = v.y; }
+bvec2 bvec2_join(bool x, bool y){ return bvec2(x,y); }
+
+void bvec3_split(bvec3 v, out bool x, out bool y, out bool z){ x = v.x; y = v.y; z = v.z; }
+bvec3 bvec3_join(bool x, bool y, bool z){ return bvec3(x,y,z); }
+
+void bvec4_split(bvec4 v, out bool x, out bool y, out bool z, out bool w){ x = v.x; y = v.y; z = v.z; w = v.w; }
+bvec4 bvec4_join(bool x, bool y, bool z, bool w){ return bvec4(x,y,z,w); }
+
 #endif //BOOL_GLSL
