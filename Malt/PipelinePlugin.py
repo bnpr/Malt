1bbe3e565772782cc8be0be96e230de8b10c057b
pragma37 <pragma37@gmail.com>
2021-11-09 00:15:30 +0100

    draft

---
+++ Malt/PipelinePlugin.py
@@ -0,0 +1,20 @@
+
+class PipelinePlugin(object):
+    
+    def __init__(self):
+        pass
+
+    def on_scene_setup(self):
+        pass
+    
+    def on_render_begin(self):
+        pass
+
+    def on_resize(self):
+        pass
+
+    def bind_scene_pass(self):
+        pass
+
+    def bind_screen_pass(self):
+        pass
