6ca5d7aa9e418bd4aa79f6caa9451480a64766d5
pragma37 <pragma37@gmail.com>
2021-11-17 19:35:17 +0100

    cleanup

--- Malt/__init__.py
+++ Malt/__init__.py
@@ -1,12 +1,2 @@
 # Copyright (c) 2020 BlenderNPR and contributors. MIT license. 
 
-'''
-from Malt import Pipeline, PipelineGraph, PipelineNode, PipelineParameters, PipelinePlugin, Scene, SourceTranspiler, Utils
-
-from Malt import GL, Library, Pipelines, Render
-
-modules = [
-    Pipeline, PipelineGraph, PipelineNode, PipelineParameters, PipelinePlugin, Scene, SourceTranspiler, Utils, 
-    GL, Library, Pipelines, Render
-]
-'''
