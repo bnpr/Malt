98085fa9f6658f83d134f33c354b555de0d1f4b1
pragma37 <pragma37@gmail.com>
2022-01-13 19:03:33 +0100

    dependencies license

---
+++ scripts/generate_license_dependencies_full.py
@@ -0,0 +1,24 @@
+import os
+
+TOP_DIR = os.path.join(os.path.dirname(__file__), os.pardir)
+
+dependencies = open(os.path.join(TOP_DIR, 'LICENSE - DEPENDENCIES')).read()
+
+result = ''
+
+for dependency in dependencies.split('\n\n'):
+    lines = dependency.splitlines()
+    for i, line in enumerate(lines):
+        if i < 3:
+            result += line
+        else:
+            url = line.replace('github.com', 'raw.githubusercontent.com').replace('blob/','')
+            from urllib.request import urlopen
+            content = urlopen(url).read().decode('utf-8')
+            result+=content
+        result+='\n'
+    result += '*'*80
+    result+='\n'
+
+open(os.path.join(TOP_DIR, 'LICENSE - DEPENDENCIES (FULL TEXT)'), 'w').write(result)
+    
