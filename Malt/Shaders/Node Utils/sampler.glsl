149a2f69c53620d6ee78cde6f3aee04826bc28a9
pragma37 <pragma37@gmail.com>
2022-02-01 16:04:53 +0100

    sampler2D_sample used UV[0] by default

--- Malt/Shaders/Node Utils/sampler.glsl
+++ Malt/Shaders/Node Utils/sampler.glsl
@@ -5,7 +5,9 @@ vec4 sampler1D_sample(sampler1D t, float u) { return texture(t, u); }
 int sampler1D_size(sampler1D t) { return textureSize(t, 0); }
 vec4 sampler1D_textel_fetch(sampler1D t, int u) { return texelFetch(t, u, 0); }
 
+/* META @uv: default=UV[0];*/
 vec4 sampler2D_sample(sampler2D t, vec2 uv) { return texture(t, uv); }
+/* META @uv: default=UV[0];*/
 vec4 sampler2D_sample_nearest(sampler2D t, vec2 uv){ return texelFetch(t, ivec2(textureSize(t, 0) * uv), 0); }
 ivec2 sampler2D_size(sampler2D t) { return textureSize(t, 0); }
 vec4 sampler2D_textel_fetch(sampler2D t, ivec2 uv) { return texelFetch(t, uv, 0); }
