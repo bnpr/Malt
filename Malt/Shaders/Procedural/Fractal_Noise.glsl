dcdde6b9b0fe0d96717f610e8717e5bc1ac1737e
Kolupsy <friedrich@grattenthaler.de>
2022-07-11 12:11:01 +0200

    fix fractal noise avg strength

--- Malt/Shaders/Procedural/Fractal_Noise.glsl
+++ Malt/Shaders/Procedural/Fractal_Noise.glsl
@@ -11,16 +11,19 @@
 vec4 fractal_noise_ex(vec4 coord, int octaves, bool tile, vec4 tile_size)
 {
     vec4 result = vec4(0);
-    float strength = 0.5;
+    float strength = 1.0;
+    float max_strength = 0.0;
 
     for (int i = 0; i < octaves; i++) 
-    {
-        result += strength * noise_ex(coord, tile, tile_size);
+    {   
+        vec4 noise = noise_ex(coord, tile, tile_size);
+        result += strength * noise;
+        max_strength += strength;
         coord *= 2.0;
         tile_size *= 2.0;
         strength *= 0.5;
     }
-    return result;
+    return result / max_strength;
 }
 
 
