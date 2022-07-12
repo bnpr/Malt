edc7b6019e40246e0fa262929e31bbdc94650be4
pragma37 <pragma37@gmail.com>
2022-07-12 16:32:14 +0200

    rename max_strength to total_strength

--- Malt/Shaders/Procedural/Fractal_Noise.glsl
+++ Malt/Shaders/Procedural/Fractal_Noise.glsl
@@ -12,18 +12,18 @@ vec4 fractal_noise_ex(vec4 coord, int octaves, bool tile, vec4 tile_size)
 {
     vec4 result = vec4(0);
     float strength = 1.0;
-    float max_strength = 0.0;
+    float total_strength = 0.0;
 
     for (int i = 0; i < octaves; i++) 
     {   
         vec4 noise = noise_ex(coord, tile, tile_size);
         result += strength * noise;
-        max_strength += strength;
+        total_strength += strength;
+        strength *= 0.5;
         coord *= 2.0;
         tile_size *= 2.0;
-        strength *= 0.5;
     }
-    return result / max_strength;
+    return result / total_strength;
 }
 
 
