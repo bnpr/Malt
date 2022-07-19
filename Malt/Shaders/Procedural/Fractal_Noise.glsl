0999ce3288c33a6ac5f4b79d418de45ec3a56088
Kolupsy <friedrich@grattenthaler.de>
2022-07-19 17:16:16 +0200

    better fractal noise

--- Malt/Shaders/Procedural/Fractal_Noise.glsl
+++ Malt/Shaders/Procedural/Fractal_Noise.glsl
@@ -4,37 +4,46 @@
 #include "Noise.glsl"
 
 /*  META GLOBAL
-    @meta: category=Texturing; subcategory=Fractal Noise;
+    @meta: category=Texturing;
 */
 
 /*  META
+    @meta: internal=true;
     @coord: subtype=Vector; default=vec4(POSITION,0);
-    @octaves: default=3;
+    @detail: default=3.0; min=1.0;
+    @roughness: subtype=Slider; min=0.0; max=1.0; default = 0.5;
     @tile_size: subtype=Vector; default=vec4(1);
 */
-vec4 fractal_noise_ex(vec4 coord, int octaves, bool tile, vec4 tile_size)
+vec4 fractal_noise_ex(vec4 coord, float detail, float roughness, bool tile, vec4 tile_size)
 {
     vec4 result = vec4(0);
-    float strength = 0.5;
+    float weight = 1.0;
+    float total_weight = 0.0;
+    roughness = clamp(roughness, 0.00001, 1.0);
+    detail = max(1.0, detail);
 
-    for (int i = 0; i < octaves; i++) 
-    {
-        result += strength * noise_ex(coord, tile, tile_size);
+    for (int i = 0; i < ceil(detail); i++) 
+    {   
+        vec4 noise = noise_ex(coord, tile, tile_size);
+        float octave_weight = (i + 1 > floor(detail))? mod(detail, 1.0) : 1.0;
+        weight *= roughness * 2 * octave_weight;
+        result += weight * noise;
+        total_weight += weight;
         coord *= 2.0;
         tile_size *= 2.0;
-        strength *= 0.5;
     }
-    return result;
+    return result / total_weight;
 }
 
 
 /*  META
+    @meta: internal=true;
     @coord: subtype=Vector; default=vec4(POSITION,0);
-    @octaves: default=3;
+    @detail: default=3;
 */
-vec4 fractal_noise(vec4 coord, int octaves)
+vec4 fractal_noise(vec4 coord, float detail, float roughness)
 {
-    return fractal_noise_ex(coord, octaves, false, vec4(0));
+    return fractal_noise_ex(coord, detail, roughness, false, vec4(0));
 }
 
 #endif // PROCEDURAL_FRACTAL_NOISE_GLSL
