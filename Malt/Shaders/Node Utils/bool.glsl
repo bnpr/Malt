39011342519b6601ab163c92bd43e8b677a0b8ec
pragma37 <pragma37@gmail.com>
2022-07-19 23:12:29 +0200

    Boolean Operations update

--- Malt/Shaders/Node Utils/bool.glsl
+++ Malt/Shaders/Node Utils/bool.glsl
@@ -2,14 +2,18 @@
 #define BOOL_GLSL
 
 /*  META GLOBAL
-    @meta: category=Math; subcategory=Bool;
+    @meta: category=Math; subcategory=Boolean Operations;
 */
 
+/*META @meta: label=And;*/
 bool bool_and(bool a, bool b) { return a && b; }
+/*META @meta: label=Or;*/
 bool bool_or(bool a, bool b) { return a || b; }
+/*META @meta: label=Not;*/
 bool bool_not(bool b) { return !b; }
-
+/*META @meta: label=Equal;*/
 bool bool_equal(bool a, bool b){ return a == b; }
+/*META @meta: label=Not Equal;*/
 bool bool_not_equal(bool a, bool b){ return a != b; }
 
 bool if_else(bool condition, bool if_true, bool if_false){ return condition ? if_true : if_false; }
