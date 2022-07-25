e049137544e241af49b4095d0cb1500cbb64f563
Kolupsy <friedrich@grattenthaler.de>
2022-07-25 15:31:06 +0200

    add mapping node

--- Malt/Shaders/Node Utils/Vector.glsl
+++ Malt/Shaders/Node Utils/Vector.glsl
@@ -59,4 +59,55 @@ void Transform(
     }
 }
 
+/* META
+    @meta: subcategory=Mapping; label=Point;
+    @vector: subtype=Vector; default='vec3(0.0)';
+    @location: subtype=Vector;
+    @rotation: subtype=Euler;
+    @scale: subtype=Vector; default=vec3(1.0);
+*/
+vec3 mapping_point(vec3 vector, vec3 location, vec3 rotation, vec3 scale)
+{
+    vec3 result = vector * scale;
+    result = transform_point(mat4_rotation_from_euler(rotation), result);
+    return result + location;
+}
+
+/* META
+    @meta: subcategory=Mapping; label=Texture;
+    @vector: subtype=Vector; default='vec3(0.0)';
+    @location: subtype=Vector;
+    @rotation: subtype=Euler;
+    @scale: subtype=Vector; default=vec3(1.0);
+*/
+vec3 mapping_texture(vec3 vector, vec3 location, vec3 rotation, vec3 scale)
+{
+    vec3 result = vector - location;
+    result = transform_point(inverse(mat4_rotation_from_euler(rotation)), result);
+    return result / scale;
+}
+
+/* META
+    @meta: subcategory=Mapping; label=Vector;
+    @vector: subtype=Vector; default='vec3(0.0)';
+    @rotation: subtype=Euler;
+    @scale: subtype=Vector; default=vec3(1.0);
+*/
+vec3 mapping_vector(vec3 vector, vec3 rotation, vec3 scale)
+{
+    return transform_direction(mat4_rotation_from_euler(rotation), vector * scale);
+}
+
+/* META
+    @meta: subcategory=Mapping; label=Normal;
+    @vector: subtype=Vector; default='vec3(0.0)';
+    @rotation: subtype=Euler;
+    @scale: subtype=Vector; default=vec3(1.0);
+*/
+
+vec3 mapping_normal(vec3 vector, vec3 rotation, vec3 scale)
+{
+    return normalize(mapping_vector(vector, rotation, scale));
+}
+
 #endif // COMMON_VECTOR_GLSL
