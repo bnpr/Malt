e73555f10171f6b232824a128aece8e0435e00f0
Kolupsy <friedrich@grattenthaler.de>
2022-08-06 22:30:00 +0200

    update sharpen filter to use blur internally

--- Malt/Shaders/Filters/Sharpen.glsl
+++ Malt/Shaders/Filters/Sharpen.glsl
@@ -1,26 +1,19 @@
 #ifndef SHARPEN_GLSL
 #define SHARPEN_GLSL
 
+#include "Filters/Blur.glsl"
+
 /* META
     @meta: category=Filter;
     @uv: default = UV[0];
-    @sharpness: default = 0.1; min=0.0;
+    @sharpness: default = 0.3; min=0.0;
 */
 vec4 sharpen(sampler2D tex, vec2 uv, float sharpness)
 {
     vec4 base = texture(tex, uv);
-    if(sharpness <= 0.0){ return base; }
-
-    vec2 tex_size = textureSize(tex, 0);
-    vec3 neighbors;
-
-    vec2 offsets[4] = vec2[]( vec2(0, 1), vec2(0, -1), vec2(1, 0), vec2(-1, 0));
-    for(int i = 0; i < 4; ++i)
-    {
-        neighbors += texture(tex, uv + offsets[i] / tex_size).rgb * vec3(-sharpness);
-    }
-    base.rgb += (base.rgb * vec3(sharpness * 4)) + neighbors;
-    return base;
+    vec4 blurred_base = box_blur(tex, uv, 1, true);
+    float scaler = 10.0; // Use scalar to put the range of the sharpness value mostly between 0-1 for convenience
+    return base + (base - blurred_base) * sharpness * scaler;
 }
 
 #endif //SHARPEN_GLSL
