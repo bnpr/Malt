35bf0cfe2149a7314f3cfab78b0a675aaff111bf
pragma37 <pragma37@gmail.com>
2022-08-09 18:01:47 +0200

    Rename roughness to detail_balance
    Only for the internal function

--- Malt/Shaders/Procedural/Fractal_Noise.glsl
+++ Malt/Shaders/Procedural/Fractal_Noise.glsl
@@ -10,22 +10,22 @@
 /*  META
     @coord: subtype=Vector; default=vec4(POSITION,0);
     @detail: default=3.0; min=1.0;
-    @roughness: subtype=Slider; min=0.0; max=1.0; default = 0.5;
+    @detail_balance: subtype=Slider; min=0.0; max=1.0; default = 0.5;
     @tile_size: subtype=Vector; default=vec4(1);
 */
-vec4 fractal_noise_ex(vec4 coord, float detail, float roughness, bool tile, vec4 tile_size)
+vec4 fractal_noise_ex(vec4 coord, float detail, float detail_balance, bool tile, vec4 tile_size)
 {
     vec4 result = vec4(0);
     float weight = 1.0;
     float total_weight = 0.0;
-    roughness = clamp(roughness, 0.00001, 1.0);
+    detail_balance = clamp(detail_balance, 0.00001, 1.0);
     detail = max(1.0, detail);
 
     for (int i = 0; i < ceil(detail); i++) 
     {   
         vec4 noise = noise_ex(coord, tile, tile_size);
         float octave_weight = (i + 1 > floor(detail))? mod(detail, 1.0) : 1.0;
-        weight *= roughness * 2 * octave_weight;
+        weight *= detail_balance * 2 * octave_weight;
         result += weight * noise;
         total_weight += weight;
         coord *= 2.0;
