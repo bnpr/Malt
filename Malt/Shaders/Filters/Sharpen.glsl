b58e3a6f36ac39e790246e9b9525320a1ec5e931
pragma37 <pragma37@gmail.com>
2022-08-10 17:08:44 +0200

    Update Sharpen.glsl

--- Malt/Shaders/Filters/Sharpen.glsl
+++ Malt/Shaders/Filters/Sharpen.glsl
@@ -3,17 +3,55 @@
 
 #include "Filters/Blur.glsl"
 
-/* META
-    @meta: category=Filter;
-    @uv: default = UV[0];
-    @sharpness: default = 0.3; min=0.0;
+/*  META GLOBAL
+    @meta: category=Filter; subcategory=Sharpen;
 */
-vec4 sharpen(sampler2D tex, vec2 uv, float sharpness)
+
+vec4 _sharpen_common(sampler2D tex, vec2 uv, vec4 blurred, float sharpness)
 {
     vec4 base = texture(tex, uv);
-    vec4 blurred_base = box_blur(tex, uv, 1, true);
     float scaler = 10.0; // Use scalar to put the range of the sharpness value mostly between 0-1 for convenience
-    return base + (base - blurred_base) * sharpness * scaler;
+    return base + (base - blurred) * sharpness * scaler;
 }
+
+/* META
+    @meta: label=Box;
+    @uv: default = UV[0];
+    @radius: default=1.0; min=0.0;
+    @sharpness: default = 0.3; min=0.0;
+*/
+vec4 box_sharpen(sampler2D tex, vec2 uv, float radius, bool circular, float sharpness)
+{
+    vec4 blurred = box_blur(tex, uv, radius, circular);
+    return _sharpen_common(tex, uv, blurred, sharpness);
+}
+
+/* META
+    @meta: label=Gaussian;
+    @uv: default = UV[0];
+    @radius: default=1.0; min=0.0;
+    @sigma: default=1.0;
+    @sharpness: default = 0.3; min=0.0;
+*/
+vec4 gaussian_sharpen(sampler2D tex, vec2 uv, float radius, float sigma, float sharpness)
+{
+    vec4 blurred = gaussian_blur(tex, uv, radius, sigma);
+    return _sharpen_common(tex, uv, blurred, sharpness);
+}
+
+/* META
+    @meta: label=Jitter;
+    @uv: default = UV[0];
+    @radius: default=1.0; min=0.0;
+    @distribution_exponent: default=5.0;
+    @samples: default=8; min=1;
+    @sharpness: default = 0.3; min=0.0;
+*/
+vec4 jitter_sharpen(sampler2D tex, vec2 uv, float radius, float distribution_exponent, int samples, float sharpness)
+{
+    vec4 blurred = jitter_blur(tex, uv, radius, distribution_exponent, samples);
+    return _sharpen_common(tex, uv, blurred, sharpness);
+}
+
 
 #endif //SHARPEN_GLSL
