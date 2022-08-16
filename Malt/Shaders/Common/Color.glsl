6453ba57dc3a75ac10cc8b8d3154553f68a9e3fd
pragma37 <pragma37@gmail.com>
2022-08-16 16:21:27 +0200

    fix hsv to rgb

--- Malt/Shaders/Common/Color.glsl
+++ Malt/Shaders/Common/Color.glsl
@@ -79,7 +79,7 @@ vec3 hsv_to_rgb(vec3 hsv)
 {
     vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
     vec3 p = abs(fract(hsv.xxx + K.xyz) * 6.0 - K.www);
-    return srgb_to_linear(hsv.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), hsv.y));
+    return srgb_to_linear(hsv.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), max(hsv.y,0)));
 }
 
 /* META @meta: label=HSV Edit; */
