a285fb9973c92d8b5b0999121a001b4b24ab5a29
pragma37 <pragma37@gmail.com>
2022-09-02 18:42:51 +0200

    fix uvec ivec eval

--- Malt/GL/GLSLEval.py
+++ Malt/GL/GLSLEval.py
@@ -1,11 +1,11 @@
-def glsl_vector(length, *args):
+def glsl_vector(convert, length, *args):
     unpacked_args = []
     for arg in args:
         try:
             unpacked_args.extend([*arg])
         except:
             unpacked_args.append(arg)
-    unpacked_args = [float(arg) for arg in unpacked_args]
+    unpacked_args = [convert(arg) for arg in unpacked_args]
     if len(unpacked_args) == 0:
         return (0.0)*length
     elif len(unpacked_args) == 1:
@@ -14,22 +14,23 @@ def glsl_vector(length, *args):
         assert(len(unpacked_args) == length)
         return tuple(unpacked_args)
 
-def vec2(*args):
-    return glsl_vector(2, *args)
+def _vec2(convert, *args): return glsl_vector(convert, 2, *args)
+def _vec3(convert, *args): return glsl_vector(convert, 3, *args)
+def _vec4(convert, *args): return glsl_vector(convert, 4, *args)
 
-def vec3(*args):
-    return glsl_vector(3, *args)
+def vec2(*args): return _vec2(float, *args)
+def vec3(*args): return _vec3(float, *args)
+def vec4(*args): return _vec4(float, *args)
 
-def vec4(*args):
-    return glsl_vector(4, *args)
+def ivec2(*args): return _vec2(int, *args)
+def ivec3(*args): return _vec3(int, *args)
+def ivec4(*args): return _vec4(int, *args)
 
-ivec2 = vec2
-ivec3 = vec3
-ivec4 = vec4
+def uint(n): return max(int(n), 0)
 
-uvec2 = vec2
-uvec3 = vec3
-uvec4 = vec4
+def uvec2(*args): return _vec2(uint, *args)
+def uvec3(*args): return _vec3(uint, *args)
+def uvec4(*args): return _vec4(uint, *args)
 
 def glsl_eval(str):
     true = True
