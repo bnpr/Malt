c9ca67a690436c3f883bf490bad2bc8ccf01ea57
pragma37 <pragma37@gmail.com>
2022-09-04 23:22:35 +0200

    Fallback for wrong installation

---
+++ __init__.py
@@ -0,0 +1,20 @@
+# Some people are used to download the zipped repo from Github to install Blender addons.
+# This file is just to redirect those users to the right path. It's not distributed in the actual addon.
+
+bl_info = {
+    "name": "Oops! You downloaded the wrong BlenderMalt file.",
+    "description" : "Please, read the install intructions on Github or malt3d.com",
+    "author" : "Miguel Pozo",
+    "version": (1,0,0),
+    "blender" : (3, 0, 0),
+    "doc_url": "https://malt3d.com/documentation/getting started/#install",
+    "tracker_url": "https://github.com/bnpr/Malt#install",
+    "category": "Render"
+}
+
+def register():
+    pass
+
+def unregister():
+    pass
+
