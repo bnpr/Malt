cbb4dc0101814eb89643e240565c38c06d9c9307
Kolupsy <kolupsy@gmail.com>
2022-10-08 17:01:41 +0200

    add new UI options to create node groups

--- BlenderMalt/MaltNodes/MaltNodeUITools.py
+++ BlenderMalt/MaltNodes/MaltNodeUITools.py
@@ -342,12 +342,83 @@ class NODE_OT_add_malt_subcategory_node(bpy.types.Operator):
             bpy.ops.wm.malt_cycle_sub_categories('INVOKE_DEFAULT')
         return{'FINISHED'}
 
+class OT_MaltAddNodeGroup(bpy.types.Operator):
+    bl_idname = 'wm.malt_add_node_group'
+    bl_label = 'Add Node Group'
+    bl_description = 'Add a new node group to a group node'
+    bl_options = {'UNDO'}
+
+    @classmethod
+    def poll(cls, context: bpy.types.Context):
+        from BlenderMalt.MaltNodes.Nodes import MaltGroupNode
+        return (
+            is_malt_tree_context(context)
+            and isinstance(context.active_node, MaltGroupNode.MaltGroupNode)
+        )
+
+    def execute(self, context: bpy.types.Context):
+        from BlenderMalt.MaltNodes.Nodes import MaltGroupNode
+        node: MaltGroupNode.MaltGroupNode = context.active_node
+        node.create_new_group()
+        return {'FINISHED'}
+    
+    @classmethod
+    def draw_ui(cls, layout: bpy.types.UILayout, node: bpy.types.Node, **layout_args):
+        layout.context_pointer_set('active_node', node)
+        layout.operator(cls.bl_idname, **layout_args)
+
+class OT_MaltNodesToGroup(bpy.types.Operator):
+    bl_idname = 'wm.malt_nodes_to_group'
+    bl_label = 'Convert Nodes to Group'
+    bl_description = 'Copies selected nodes and converts them into a new node group'
+
+    @classmethod
+    def poll(cls, context: bpy.types.Context):
+        return (
+            is_malt_tree_context(context)
+            and len(context.selected_nodes) > 0
+        )
+    
+    def execute(self, context: bpy.types.Context):
+        from BlenderMalt.MaltNodes.Nodes.MaltGroupNode import MaltGroupNode
+        from BlenderMalt.MaltNodes.MaltNodeTree import set_node_tree
+        from mathutils import Vector
+        sd: bpy.types.SpaceNodeEditor = context.space_data
+        selected: list[bpy.types.Node] = context.selected_nodes
+        avg_loc = Vector((0.0, 0.0))
+        for node in selected:
+            avg_loc += Vector(get_absolute_node_position(node))
+        avg_loc /= len(selected)
+
+        bpy.ops.node.clipboard_copy()
+        for n in selected:
+            sd.edit_tree.nodes.remove(n)
+        
+        new_group_node: MaltGroupNode = sd.edit_tree.nodes.new(MaltGroupNode.bl_idname)
+        new_nt = new_group_node.create_new_group()
+        new_group_node.location = avg_loc
+        sd.edit_tree.active = new_group_node
+        for n in sd.edit_tree.nodes:
+            n.select = n == new_group_node
+        set_node_tree(context, new_nt, new_group_node)
+        bpy.ops.node.clipboard_paste()
+        for n in new_nt.nodes.values():
+            n.location = Vector(n.location) - avg_loc
+        return {'FINISHED'}
+
+def get_absolute_node_position(node: bpy.types.Node):
+    if node.parent is None:
+        return node.location
+    return get_absolute_node_position(node.parent) + node.location
+
 classes = [
     NodeTreePreview,
     OT_MaltEditNodeTree,
     OT_MaltSetTreePreview,
     OT_MaltConnectTreePreview,
     OT_MaltCycleSubCategories,
+    OT_MaltAddNodeGroup,
+    OT_MaltNodesToGroup,
     NODE_OT_add_malt_subcategory_node,
 ]
 
@@ -443,6 +514,7 @@ def register_node_tree_shortcuts():
         add_shortcut(keymaps, OT_MaltEditNodeTree, type='TAB', value='PRESS')
         add_shortcut(keymaps, OT_MaltSetTreePreview, type='LEFTMOUSE', value='PRESS', shift=True, alt=True)
         add_shortcut(keymaps, OT_MaltConnectTreePreview, type='LEFTMOUSE', value='PRESS', shift=True, ctrl=True)
+        add_shortcut(keymaps, OT_MaltNodesToGroup, type='G', value='PRESS', ctrl=True)
 
 def register():
     for _class in classes: bpy.utils.register_class(_class)
--- BlenderMalt/MaltNodes/Nodes/MaltGroupNode.py
+++ BlenderMalt/MaltNodes/Nodes/MaltGroupNode.py
@@ -1,22 +1,51 @@
 from Malt.PipelineParameters import Parameter, Type
 import bpy    
 from BlenderMalt.MaltNodes.Nodes.MaltFunctionNode import MaltFunctionNodeBase
+from BlenderMalt.MaltNodes.MaltNodeTree import MaltTree
 
 
 class MaltGroupNode(bpy.types.Node, MaltFunctionNodeBase):
-    
+
+    bl_idname = 'MaltGroupNode'
     bl_label = "Group Node"
 
-    def poll_group(self, tree):
-        group_type = self.id_data.graph_type
-        if group_type.endswith(' (Group)') == False:
-            group_type += ' (Group)'
-        return tree.bl_idname == 'MaltTree' and tree.graph_type == group_type
+    def poll_group(self, tree: MaltTree):
+        group_type = self.get_graph_type_from_parent(self.id_data)
+
+        def is_target_nested_in_source(target: MaltTree, source: MaltTree):
+            if source is None:
+                return False
+            for node in (n for n in source.nodes.values() if isinstance(n, MaltGroupNode)):
+                if node.group is target or is_target_nested_in_source(node.group, target):
+                    return True
+            return False
+
+        return (
+            tree.bl_idname == 'MaltTree' 
+            and tree.graph_type == group_type
+            and not tree == self.id_data
+            and not is_target_nested_in_source(self.id_data, tree)
+        )
+    
+    @staticmethod
+    def get_graph_type_from_parent(malt_tree: MaltTree) -> str:
+        graph_type: str = malt_tree.graph_type
+        suffix = ' (Group)'
+        if not graph_type.endswith(suffix):
+            graph_type += suffix
+        return graph_type
     
     def update_group(self, context):
         self.setup(context)
+        self.setup_width() #has to be called explicitely because 'setup' will only call 'setup_width' in some cases
 
     group : bpy.props.PointerProperty(type=bpy.types.NodeTree, poll=poll_group, update=update_group)
+
+    def create_new_group(self) -> MaltTree:
+        nt = bpy.data.node_groups.new('Malt Node Tree', 'MaltTree')
+        nt.graph_type = self.get_graph_type_from_parent(self.id_data)
+        self.group = nt
+        return nt
 
     def get_linked_node_tree(self):
         return self.group
@@ -30,13 +59,26 @@ class MaltGroupNode(bpy.types.Node, MaltFunctionNodeBase):
 
     def draw_buttons(self, context, layout):
         from BlenderMalt.MaltNodes.MaltNodeTree import set_node_tree
+        from BlenderMalt.MaltNodes.MaltNodeUITools import OT_MaltAddNodeGroup
         row = layout.row(align=True)
-        row.prop(self, 'group', text='')
+        row.template_ID(self, 'group', new=OT_MaltAddNodeGroup.bl_idname)
         if self.group is not None:
             row.operator('wm.malt_callback', text = '', icon = 'GREASEPENCIL').callback.set(
                 lambda: set_node_tree(context, self.group, self)
             )
     
+    def calc_node_width(self, point_size, dpi) -> float:
+        import blf
+        blf.size(0, point_size, dpi)
+
+        button_padding = 150 #Magic number to account for the other buttons on the node UI
+        if getattr(self.group, 'users', 0) > 1:
+            button_padding += 30
+
+        width = super().calc_node_width(point_size, dpi)
+        width = max(width, blf.dimensions(0, getattr(self.group, 'name', ''))[0] + button_padding)
+        return width
+
 classes = [
     MaltGroupNode,
 ]
