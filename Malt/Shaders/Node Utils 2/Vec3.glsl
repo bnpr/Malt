25bb38593f58eb4bce73134c915911f4ed475702
Miguel Pozo <pragma37@gmail.com>
2023-02-25 19:34:23 +0100

    safe normalize, length & distance

--- Malt/Shaders/Node Utils 2/Vec3.glsl
+++ Malt/Shaders/Node Utils 2/Vec3.glsl
@@ -73,12 +73,12 @@ vec3 Vec3_mix(vec3 a, vec3 b, vec3 c){ return mix(a,b,c); }
 vec3 Vec3_mix_float(vec3 a, vec3 b, float fac){ return mix(a,b,fac); }
 
 /*META @meta: label=Normalize; @a: subtype=Vector;*/
-vec3 Vec3_normalize(vec3 a){ return normalize(a); }
+vec3 Vec3_normalize(vec3 a){ return a != 0 ? normalize(a) : vec3(0); }
 
 /*META @meta: label=Length; @a: subtype=Vector;*/
-float Vec3_length(vec3 a){ return length(a); }
+float Vec3_length(vec3 a){ return a != 0 ? length(a) : 0; }
 /*META @meta: label=Distance; @a: subtype=Vector; @b: subtype=Vector;*/
-float Vec3_distance(vec3 a, vec3 b){ return distance(a,b); }
+float Vec3_distance(vec3 a, vec3 b){ return a != b ? distance(a,b) : 0; }
 /*META @meta: label=Dot Product; @a: subtype=Vector; @b: subtype=Vector;*/
 float Vec3_dot_product(vec3 a, vec3 b){ return dot(a,b); }
 /*META @meta: label=Cross Product; @a: subtype=Vector; @b: subtype=Vector;*/
